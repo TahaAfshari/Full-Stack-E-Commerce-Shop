b513cc66b9ffc902d0ba12320425ece9de3e872b
TahaAfshari <taha0afshari@gmail.com>
2024-06-06 13:32:11 +0330

    First Phase complete

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+"use strict";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`mongoDB connection successful`)
+    }
+
+    catch (error) {
+        console.error(`ERROR: {error.message}`)
+        process.exit(1)
+    }
+};
+
+export default connectDB;
+
